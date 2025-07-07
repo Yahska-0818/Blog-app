2e29a130526c2f01f4a6193a13bbabe503d5e77e
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-07 08:06:52 +0000

    working now

--- src/Create.js
+++ src/Create.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
   const [isPending, setIsPending] = useState(false);
-  const history = useHistory();
+  const history = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
