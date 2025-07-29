fa0c1fe44a968af914e975a2ffe1ba57efe438e3
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-29 18:18:43 +0000

    made changes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
 
